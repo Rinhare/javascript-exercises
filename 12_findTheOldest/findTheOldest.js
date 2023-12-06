672023905bd9cb413be924a861b93045c2668a95
Jusu <jusurinhare@gmail.com>
2023-12-06 17:31:02 +0200

    all exercises done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
 
+const findTheOldest = function(oldest) {
+let oldestPerson = null;
+let oldestAge = 0;
+
+for(i = 0; i < oldest.length; i++) {
+
+    const person = oldest[i];
+
+    const age = person.yearOfDeath - person.yearOfBirth || new Date().getFullYear() - person.yearOfBirth;
+
+    if (age > oldestAge) {
+    oldestPerson = person;
+    oldestAge = age;
+    }
+
+
+}
+return oldestPerson;
 };
 
 // Do not edit below this line
