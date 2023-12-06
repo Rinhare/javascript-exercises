4df390caca220a7a12d411a4c0129e676ea5632c
Jusu <jusurinhare@gmail.com>
2023-12-06 14:07:46 +0200

    fibonacci done with recursive function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+ if(num < 0) {
+    return "OOPS";
+ }   
+ 
+if(num == "1") {
+    return 1
+}
 
+
+ if(num == isNaN) {
+  num = parseInt(num);
+ }
+
+if (num <= 1) {
+    return num;
+}
+
+return fibonacci(num - 1) + fibonacci(num - 2);
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
